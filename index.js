36bfb1cfa7b4a0478ab19b6a8b4a6edb822486fb
Tom Fenech <tomjwfenech@gmail.com>
2018-01-23 20:21:09 +0100

    Use a separate polling interval for each host

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const util = require('util');
 const timers = require('timers');
 const WebSocket = require('ws');
 const ping = require('net-ping');
@@ -8,6 +9,43 @@ const wss = new WebSocket.Server({ port: 8080 });
 let connectionId = 0;
 const allClients = {};
 const allHostnames = {};
+
+function sendToSubscribers(hostname, error) {
+    const message = JSON.stringify({ hostname, error });
+
+    Array.from(allHostnames[hostname].subscribers).forEach(id => {
+        const client = allClients[id];
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(message);
+        }
+        console.error(error);
+    });
+}
+
+const dnsLookup = util.promisify(dns.lookup);
+const pingSession = ping.createSession();
+const pingHost = util.promisify(pingSession.pingHost.bind(pingSession));
+
+async function poll(hostname) {
+    try {
+        const { address } = await dnsLookup(hostname);
+
+        try {
+            await pingHost(address);
+            allHostnames[hostname].fails = 0;
+        } catch (error) {
+            ++allHostnames[hostname].fails;
+
+            if (allHostnames[hostname].fails >= 5) {
+                throw error;
+            }
+        }
+    } catch (error) {
+        sendToSubscribers(hostname, error);
+    }
+
+    timers.setTimeout(() => poll(hostname), 1000);
+}
 
 function addMessageHandler(client, connectionId) {
     client.on('message', function incoming(message) {
@@ -20,6 +58,8 @@ function addMessageHandler(client, connectionId) {
                             fails: 0,
                             subscribers: new Set()
                         };
+
+                        poll(hostname);
                     }
 
                     allHostnames[hostname].subscribers.add(connectionId);
@@ -36,41 +76,3 @@ wss.on('connection', function connection(ws) {
     addMessageHandler(ws, connectionId);
     ++connectionId;
 });
-
-const pingSession = ping.createSession();
-
-function sendToSubscribers(hostname, problem, error) {
-    const message = JSON.stringify({ hostname, problem, error });
-
-    Array.from(allHostnames[hostname].subscribers).forEach(id => {
-        const client = allClients[id];
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(message);
-        }
-        console.error(error);
-    });
-}
-
-(function pingHosts() {
-    Object.keys(allHostnames).forEach(hostname => {
-        dns.lookup(hostname, (error, address, family) => {
-            if (error) {
-                sendToSubscribers(hostname, 'could not resolve', error);
-                return;
-            }
-
-            pingSession.pingHost(address, (error) => {
-                if (error) {
-                    ++allHostnames[hostname].fails;
-                } else {
-                    allHostnames[hostname].fails = 0;
-                }
-
-                if (allHostnames[hostname].fails >= 5) {
-                    sendToSubscribers(hostname, 'ping failed', error);
-                }
-            });
-        });
-    });
-    timers.setTimeout(pingHosts, 1000);
-})();
